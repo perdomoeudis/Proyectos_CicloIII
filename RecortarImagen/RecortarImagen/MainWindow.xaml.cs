66d453adcfceedb6d9a79bee824397eeaf19bf13
Eudis Perdomo <eperdomo@t-idsolutions.com>
2018-11-20 08:30:52 -0500

    Proyecto Recortar Imagen

---
+++ RecortarImagen/RecortarImagen/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace RecortarImagen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();            
+        }
+
+        private void saveJpeg(string path, System.Drawing.Image img, long quality)
+        {
+            // Encoder parameter for image quality
+            EncoderParameter qualityParam = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, quality);
+
+            // Jpeg image codec
+            ImageCodecInfo jpegCodec = this.getEncoderInfo("image/jpeg");
+
+            if (jpegCodec == null)
+                return;
+
+            EncoderParameters encoderParams = new EncoderParameters(1);
+            encoderParams.Param[0] = qualityParam;
+
+            img.Save(path, jpegCodec, encoderParams);
+        }
+
+        private ImageCodecInfo getEncoderInfo(string mimeType)
+        {
+            // Get image codecs for all image formats
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+
+            // Find the correct image codec
+            for (int i = 0; i < codecs.Length; i++)
+                if (codecs[i].MimeType == mimeType)
+                    return codecs[i];
+            return null;
+        }
+
+        private static System.Drawing.Image cropImage(System.Drawing.Image img, System.Drawing.Rectangle cropArea)
+        {
+            Bitmap bmpImage = new Bitmap(img);
+            Bitmap bmpCrop = bmpImage.Clone(cropArea,
+            bmpImage.PixelFormat);
+            return (System.Drawing.Image)(bmpCrop);
+        }
+
+        private static System.Drawing.Image resizeImage(System.Drawing.Image imgToResize, System.Drawing.Size size)
+        {
+            int sourceWidth = imgToResize.Width;
+            int sourceHeight = imgToResize.Height;
+
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+
+            nPercentW = ((float)size.Width / (float)sourceWidth);
+            nPercentH = ((float)size.Height / (float)sourceHeight);
+
+            if (nPercentH < nPercentW)
+                nPercent = nPercentH;
+            else
+                nPercent = nPercentW;
+
+            int destWidth = (int)(sourceWidth * nPercent);
+            int destHeight = (int)(sourceHeight * nPercent);
+
+            Bitmap b = new Bitmap(destWidth, destHeight);
+            Graphics g = Graphics.FromImage((System.Drawing.Image)b);
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+
+            g.DrawImage(imgToResize, 0, 0, destWidth, destHeight);
+            g.Dispose();
+
+            return (System.Drawing.Image)b;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {   
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            System.Windows.Media.Imaging.BmpBitmapEncoder bbe = new BmpBitmapEncoder();
+            bbe.Frames.Add(BitmapFrame.Create((BitmapSource)img1.Source));
+            bbe.Save(ms);
+            System.Drawing.Image imgM1 = System.Drawing.Image.FromStream(ms);
+            System.Drawing.Image imgM2 = cropImage(imgM1, new System.Drawing.Rectangle(int.Parse(txtX.Text), int.Parse(txtY.Text), int.Parse(txtW.Text), int.Parse(txtH.Text)));   
+            img2.Source = BitmapToBitmapImage(imgM2);
+            //saveJpeg(@"D:\re.jpg", imgM2, long.Parse(txtQa.Text));
+        }
+
+        private BitmapImage BitmapToBitmapImage(System.Drawing.Image img)
+        {
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            BitmapImage bi = new BitmapImage();
+            img.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            bi.BeginInit();
+            bi.StreamSource = ms;
+            bi.EndInit();
+            return bi;
+        }
+        bool active = false;
+
+    }
+}
---
+++ RecortarImagen/RecortarImagen/test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace RecortarImagen
+{
+    class test
+    {
+        Bitmap ddd = new Bitmap(@"C:\Documents and Settings\All Users\" + @"Documents\My Music\music.bmp", true);
+    }
+}
